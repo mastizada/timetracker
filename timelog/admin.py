85e89e98eff1f0c3eea190ef4347340abe33f8c0
Emin Mastizada <emin@mastizada.com>
2016-09-15 04:24:02 +0300

    timelogs list and filters in admin

--- timelog/admin.py
+++ timelog/admin.py
@@ -4,7 +4,8 @@ from timelog.models import TimeLog
 
 class TimelogAdmin(admin.ModelAdmin):
     """Timelog in Django Admin."""
-    pass
+    list_display = ('__str__', 'started_at', 'ended_at', )
+    list_filter = ('project', 'ended_at', )
 
 
 admin.site.register(TimeLog, TimelogAdmin)
